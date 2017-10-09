3bb1f7d7e7a8c72667888797e6c9da3795a7e57d
Richard Lupton <richard.lupton@gmail.com>
2017-10-09 16:02:11 +0100

    Rewrite nix expression to use custom configuration.nix

--- nxe-netboot/build-nxe-build-image.sh
+++
@@ -1,4 +0,0 @@
-export PATH=${coreutils}/bin:${sed}/bin
-mkdir -p $out/bin
-sed "s|RELEASE_NIX_PLACEHOLDER|${nixpkgs}/nixos/release.nix|" $src > $out/bin/nxe-build-image
-chmod +x $out/bin/nxe-build-image
--- nxe-netboot/custom-image.nix
+++ nxe-netboot/custom-image.nix
@@ -1,31 +1,50 @@
 # nix expression for custom netboot image
 { pkgs ? import <nixpkgs> {} }:
 let
-  image = pkgs.netboot;
-  coreutils = pkgs.coreutils;
-  sed = pkgs.gnused;
-in rec {
-  nixpkgs = derivation {
-    name = "nixpkgs";
-    src = pkgs.fetchFromGitHub {
+  nixpkgs = pkgs.fetchFromGitHub {
       owner = "NixOS";
       repo = "nixpkgs";
       rev = "17.09";
       sha256 = "0kpx4h9p1lhjbn1gsil111swa62hmjs9g93xmsavfiki910s73sh";
-    };
-    inherit coreutils;
-    builder = "${pkgs.bash}/bin/bash";
-    args = [ ./build-nixpkgs.sh ];
-    system = builtins.currentSystem;
   };
 
-  nxe-build-image = derivation {
-    name = "nxe-build-image";
-    src = ./nxe-build-image-template;
-    inherit coreutils sed nixpkgs;
-    builder = "${pkgs.bash}/bin/bash";
-    args = [ ./build-nxe-build-image.sh ];
-    system = builtins.currentSystem;
+in rec {
+
+  build-pxe-image = { customModules ? [] }:
+    let 
+      versionSuffix = "test";
+      versionModule = {
+        system.nixosVersionSuffix = versionSuffix;
+        system.nixosRevision = nixpkgs.rev or nixpkgs.shortRev;
+      };
+
+      build = (import "${nixpkgs}/nixos/lib/eval-config.nix" {
+        system = "x86_64-linux";
+        modules = [
+          (builtins.toPath "${nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix")
+          versionModule
+        ] ++ customModules;
+      }).config.system.build;
+    in
+      pkgs.symlinkJoin {
+        name="netboot";
+        paths=[
+          build.netbootRamdisk
+          build.kernel
+          build.netbootIpxeScript
+        ];
+        postBuild = ''
+          mkdir -p $out/nix-support
+          echo "file bzImage $out/bzImage" >> $out/nix-support/hydra-build-products
+          echo "file initrd $out/initrd" >> $out/nix-support/hydra-build-products
+          echo "file ipxe $out/netboot.ipxe" >> $out/nix-support/hydra-build-products
+        '';
+      };
+
+  vanilla-netboot = build-pxe-image {};
+  test-image = build-pxe-image { 
+    customModules = [
+      ./configuration.nix
+    ];
   };
 }
-  
